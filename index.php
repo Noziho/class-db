d28ac30cc31e3a49062e113660c3c9ab6b61cc55
Noah2211 <noah.decroix3@gmail.com>
2022-02-08 13:42:04 +0100

    Singleton pattern with config file

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+require __DIR__ . '/Config.php';
+require __DIR__ . '/SingletonDB.php';
+
+
+$database = SingletonDB::dbConnect();
+$database2 = SingletonDB::dbConnect();
+var_dump($database);
+var_dump($database2);
